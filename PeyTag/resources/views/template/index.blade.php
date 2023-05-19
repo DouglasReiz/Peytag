37443c303d41e607769522fc6e1834c2be6bd1ca
Douglas Alves <douglasreisalves02@gmail.com>
2023-05-18 22:19:05 -0300

    tela inicial sendo desenvolvida e ajustes na tela de exibição de usuários

--- PeyTag/resources/views/template/index.blade.php
+++ PeyTag/resources/views/template/index.blade.php
@@ -24,8 +24,8 @@
                 <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
                     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                         <a href="https://flowbite.com" class="flex items-center">
-                            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                            <img src="https://lh6.googleusercontent.com/lt9cLDJ2LpUVX0dww2Oa8ocq73XyuKRrwlMNUW2v9rwGyLJq3s6hnk_wcau7zL9kOPY1FW0l5mCae3MRbNhfSUA=w16383" class="mr-3 h-6 sm:h-9" alt="Peytag logo" />
+                            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PeyTag</span>
                         </a>
                         <div class="flex items-center lg:order-2">
                             <a href="/login" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
--- PeyTag/resources/views/users/index.blade.php
+++ PeyTag/resources/views/users/index.blade.php
@@ -2,13 +2,13 @@
 @section('title', 'Usuários')
 @section('body')
 
-<div class="text-orange-600 text-5xl pb-4 text-center">
+<div class="text-gray-600 text-5xl pb-4 text-center">
     <h1>Listagem de Usuários</h1>
 </div>
 
 <div class="grid justify-items-center">
-    <div class="relative overflow-x-auto">
-        <table class="w-full text-sm text-left text-gray-300 dark:text-gray-100">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
                     <th class="px-6 py-3" scope="col">ID</th>
@@ -21,12 +21,12 @@
             </thead>
             <tbody>
                 @foreach($users as $user)
-                <tr onclick="window.location='#'" class="bg-orange-800 border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-white cursor-pointer">
-                    <th class="px-6 py-4 font-medium text-gray-500 whitespace-nowrap dark:text-white" scope="row">{{ $user->id }}</th>
-                    <td class="text-center px-6 py-4">{{ $user->name }}</td>
-                    <td class="text-center px-6 py-4">{{ $user->email }}</td>
-                    <td class="text-center px-6 py-4">{{ $user->phone }}</td>
-                    <td class="text-center px-6 py-4">{{ $user->is_admin }}</td>
+                <tr onclick="window.location='#'" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600">
+                    <th class="px-6 py-4 px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" scope="row">{{ $user->id }}</th>
+                    <td class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->name }}</td>
+                    <td class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->email }}</td>
+                    <td class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->phone }}</td>
+                    <td class="text-center px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{ $user->is_admin }}</td>
                     <td class="btn-alert mr-1">
                         
                     </td>
